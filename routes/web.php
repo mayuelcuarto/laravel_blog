07ebeacc372b00b539be2ca129c462cb4a45cc2a
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-04-26 19:34:42 -0500

    Cambios en web_php
    - Probando nuevas rutas.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@
 Route::get('/', function(){
 	return 'Home';
 });
+
+Route::get('/usuarios', function(){
+	return 'Usuarios';
+});
+
+Route::get('/usuarios/{id}', function($id){
+	return "Mostrando el detalle del usuario: {$id}";
+})->where('id', '[0-9]+');
+
+Route:: get('/usuarios/nuevo', function(){
+	return "Crear nuevo usuario";
+});
+
+Route::get('/saludos/{name}/{nickname?}', function($name, $nickname = null){
+	if($nickname){
+		return "Bienvenido {$name}, tu apodo es {$nickname}";
+	}else{
+		return "Bienvenido {$name}, no tienes apodo";
+	}	
+});
